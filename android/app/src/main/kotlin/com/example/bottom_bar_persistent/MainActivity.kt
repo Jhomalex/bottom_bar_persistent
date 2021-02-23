bf270f618c00fca71f293c1f0d46a7bd9b59f308
Jhonatan Malara <jhomalex07@hotmail.com>
2021-02-22 19:55:16 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bottom_bar_persistent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bottom_bar_persistent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/flight_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FlightPage extends StatelessWidget {
+  const FlightPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Vuelos"),
+    );
+  }
+}
